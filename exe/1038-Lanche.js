b820110501720df9b36687bc4106d6eef423375e
RRNunes <betanunes8@gmail.com>
2026-03-05 20:14:10 -0300

    v1

---
+++ exe/1038-Lanche.js
@@ -0,0 +1,28 @@
+/*om base na tabela abaixo, escreva um programa que leia o código de um item e a quantidade deste item.
+A seguir, calcule e mostre o valor da conta a pagar.
+
+...
+
+Entrada
+O arquivo de entrada contém dois valores inteiros correspondentes ao código e
+à quantidade de um item conforme tabela acima.
+
+Saída
+O arquivo de saída deve conter a mensagem "Total: R$ " seguido pelo valor a ser pago, 
+com 2 casas após o ponto decimal.
+
+Exemplo de Entrada	Exemplo de Saída
+  3 2                   Total: R$ 10.00
+  4 3                   Total: R$ 6.00
+  2 3                   Total: R$ 13.50  */
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+
+
